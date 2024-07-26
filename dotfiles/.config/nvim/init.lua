5a086ad7328f044164956d7600c1b46f5c68c1a6
glaumar <git@geekgo.tech>
2024-07-27 00:04:48 +0800

    Update 20240727

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,12 +1,17 @@
 if vim.g.vscode then
-    -- VSCode extension
+   -- VSCode extension
 else
     -- ordinary Neovim
-    vim.opt.cursorline = true
-    vim.opt.cursorcolumn = true
-    vim.opt.number = true
-    vim.opt.relativenumber = true
 
-    vim.o.foldmethod = 'syntax'
-    vim.o.foldenable = true
 end
+
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.foldmethod = 'syntax'
+vim.opt.foldenable = true
+
+--  add an empty line below the current line
+vim.api.nvim_set_keymap('n', '<space><space>', 'o<Esc>', { noremap = true, silent = true })
